3d0da82c033c526da2707b7f6d191c35123735ef
armyan23 <arm.davtyan1029@gmail.com>
2023-05-24 00:34:36 +0400

    Add required fields

--- models/user.js
+++ models/user.js
@@ -6,18 +6,23 @@ const userSchema = new Schema(
   {
     name: {
       type: String,
+      required: true,
     },
     email: {
       type: String,
+      required: true,
     },
     password: {
       type: String,
+      required: true,
     },
     dateOfBirthday: {
       type: Date,
+      required: true,
     },
     gender: {
       type: String,
+      required: true,
     },
     photo: {
       type: String,
