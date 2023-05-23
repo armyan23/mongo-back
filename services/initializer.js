34d9238bcd39b656400ce82186647974814c01a1
armyan23 <arm.davtyan1029@gmail.com>
2023-05-24 00:20:20 +0400

    Removed console

--- services/initializer.js
+++ services/initializer.js
@@ -2,11 +2,6 @@ const db = require("../config/index");
 const mongoose = require("mongoose");
 
 const initializeService = async (startApp) => {
-  // const response = await checkingMongodStatus();
-  // if (response?.error) {
-  //   console.log(response?.error?.message);
-  // }
-  console.log(process.env.MONGO_URI);
   db.connection = await mongoose.createConnection(process.env.MONGO_URI, {});
 
   await db.connection.once("open", () => {
